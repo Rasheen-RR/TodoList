c043d8d1f8b8cde2e54177fb2f3d800dc9402d52
Rasheen <A00229247@mycambrian.ca>
2021-04-22 19:16:46 -0400

    Added service worker and made application installable

---
+++ TodoList/wwwroot/service-worker.js
@@ -0,0 +1,29 @@
+﻿function swInstall(event) {
+    console.info("installing")
+    event.waitUntil(
+        caches.open('todov1').then((cache) => {
+            return cache.addAll([
+                './',
+                './css/site.css',
+                './js/site.js',
+                './favicon.ico',
+                './lib/bootstrap/dist/js/bootstrap.bundle.min.js',
+                './lib/jquery/dist/jquery.min.js',
+                './lib/bootstrap/dist/css/bootstrap.min.css ',
+
+            ])
+        })
+    )
+}
+
+self.addEventListener('install', swInstall);
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (cacheResponse) {
+            return cacheResponse || fetch(event.request)
+        }).catch(() => {
+            console.log("off")
+        })
+    )
+})
