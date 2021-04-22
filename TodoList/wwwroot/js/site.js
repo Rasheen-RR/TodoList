264a59531e2847d14e6894466377180ec218d712
Rasheen <A00229247@mycambrian.ca>
2021-04-22 19:17:22 -0400

    Added function to disable buttons and checkbox when browser if offline

--- TodoList/wwwroot/js/site.js
+++ TodoList/wwwroot/js/site.js
@@ -1,1 +1,16 @@
-﻿
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+    let update_status = () => {
+        if (navigator.onLine) {
+            $('.offline').css({ "pointer-events": "all" });
+            $('.check-box').attr('disabled', false);
+        } else {
+            $('.offline').css({ "pointer-events": "none" });
+            $('.check-box').attr('disabled', true);
+        }
+    }
+    window.addEventListener('online', update_status);
+    window.addEventListener('offline', update_status);
